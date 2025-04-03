7a1352a2a608897ee7300f808cd967551e2cc52a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-03 22:41:26 +0200

    Doing some updates in the app

--- healthclick_app/lib/screens/welcome/OnBoarding.dart
+++ healthclick_app/lib/screens/welcome/OnBoarding.dart
@@ -10,6 +10,24 @@ class OnBoarding extends StatefulWidget {
 class _OnBoardingState extends State<OnBoarding> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  "assets/background.jpg",
+                  width: 300,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
