24eaa07ef30950e34e8294391f93012ea82c75ee
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-04 00:56:21 +0200

    Some updates in the HomePage Widget

--- healthclick_app/lib/screens/welcome/HomePage.dart
+++ healthclick_app/lib/screens/welcome/HomePage.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
+
 }
+
+final List<String> imageList = [
+  "assets/background.jpg",
+  "assets/back1.jpg",
+  "assets/back2.jpg",
+  "assets/back3.jpg",
+];
 
 class _HomePageState extends State<HomePage> {
   @override
@@ -27,7 +36,7 @@ class _HomePageState extends State<HomePage> {
                   "Ola Mauro Peniel",
                   style: TextStyle(fontSize: 15), 
                 ),
-                subtitle: Text("O que voce deseja ?",
+                subtitle: Text("O que vce deseja ?",
                   style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),
                 ),
                 trailing:
@@ -45,17 +54,35 @@ class _HomePageState extends State<HomePage> {
                 height: 30,
               ),
               //?Image Section
-              Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.asset(
-                    "assets/background.jpg",
-                    width: 500,
-                    height: 250,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+               GFCarousel(
+                autoPlay: true, // Enable auto play
+                autoPlayInterval:
+                    Duration(seconds: 3), // Interval for auto play
+                items: imageList.map(
+                  (url) {
+                    return Container(
+                      width: 500, // Set width of image
+                      height: 250, // Set height of image
+                      margin: EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          url, // Use Image.asset for local images
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ).toList(),
+                onPageChanged: (index) {
+                  setState(() {
+                    index;
+                  });
+                },
               ),
+              const SizedBox(height: 50,),
+              const Text('Categorias',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),),
+              //?Cards Carousel
             ],
           ),        
         ),
