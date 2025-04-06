a8631132fe05935741115fa216bf7d21ad61a203
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-07 00:23:00 +0200

    Working on the pharmacy widget

--- healthclick_app/lib/main.dart
+++ healthclick_app/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: ProductDetails(),
+      home: Login(),
       // home: AppBottomNav(),
     );
   }
---
+++ healthclick_app/lib/screens/pharmacy/Pharmacy.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Pharmacy extends StatefulWidget {
+  const Pharmacy({super.key});
+
+  @override
+  State<Pharmacy> createState() => _PharmacyState();
+}
+
+class _PharmacyState extends State<Pharmacy> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
