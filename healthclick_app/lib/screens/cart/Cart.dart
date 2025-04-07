5911f75a85614757c85b78e2c31fefd5815115b2
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-07 15:50:38 +0200

    Creating a widget Cart folder and filee

---
+++ healthclick_app/lib/screens/cart/Cart.dart
@@ -0,0 +1,30 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class Cart extends StatefulWidget {
+  const Cart({super.key});
+
+  @override
+  State<Cart> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //?Main Content
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
