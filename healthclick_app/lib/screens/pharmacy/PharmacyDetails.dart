53151e853dba9cba93453855465a057e553eb30a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-07 11:08:19 +0200

    Add a bottomSheet widget in the pharmacyDetails

--- healthclick_app/lib/screens/pharmacy/PharmacyDetails.dart
+++ healthclick_app/lib/screens/pharmacy/PharmacyDetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:healthclick_app/screens/layouts/AppBottom.dart';
 
 class PharmacyDetails extends StatefulWidget {
@@ -16,6 +17,8 @@ class _PharmacyDetailsState extends State<PharmacyDetails> {
       _currentIndex = index;
     });
   }
+
+  final GFBottomSheetController _controller = GFBottomSheetController();
 
   @override
   Widget build(BuildContext context) {
@@ -69,13 +72,68 @@ class _PharmacyDetailsState extends State<PharmacyDetails> {
         currentIndex: _currentIndex,
         onTap: _onTap,
       ),
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.green,
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+      bottomSheet: GFBottomSheet(
+        controller: _controller,
+        maxContentHeight: 150,
+        stickyHeaderHeight: 100,
+        stickyHeader: Container(
+          decoration: BoxDecoration(color: Colors.white,
+              boxShadow: [BoxShadow(color: Colors.black45, blurRadius: 0)]
           ),
-          onPressed: () {}),
+          child: const GFListTile(
+            avatar: GFAvatar(
+              backgroundImage: AssetImage('assets image here'),
+            ),
+            titleText: 'GetWidget',
+            subTitleText: 'Open source UI library',
+          ),
+        ),
+        contentBody: Container(
+          height: 200,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: ListView(
+            shrinkWrap: true,
+            physics: const ScrollPhysics(),
+            children: const [
+              Center(
+                  child: Text(
+                    'Getwidget reduces your overall app development time to minimum 30% because of its pre-build clean UI widget that you can use in flutter app development. We have spent more than 1000+ hours to build this library to make flutter developer’s life easy.',
+                    style: TextStyle(
+                        fontSize: 15, wordSpacing: 0.3, letterSpacing: 0.2),
+                  ))
+            ],
+          ),
+        ),
+        stickyFooter: Container(
+          color: GFColors.SUCCESS,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Get in touch',
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                'info@getwidget.dev',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+        stickyFooterHeight: 50,
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: GFColors.SUCCESS,
+          child: _controller.isBottomSheetOpened ? Icon(Icons.keyboard_arrow_down):Icon(Icons.keyboard_arrow_up),
+          onPressed: () {
+            _controller.isBottomSheetOpened
+                ? _controller.hideBottomSheet()
+                : _controller.showBottomSheet();
+          }
+          ),
     );
   }
 }
