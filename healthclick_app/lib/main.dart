14ac0583c6778d6cd2078e0b54e0e93d9cd847ee
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-08 19:55:38 +0200

    Let me work on the createAccount widget

--- healthclick_app/lib/main.dart
+++ healthclick_app/lib/main.dart
@@ -2,8 +2,6 @@
 // import 'package:healthclick_app/models/CartProvider.dart';
 // import 'package:healthclick_app/screens/auth/Login.dart';
 // import 'package:provider/provider.dart';
-
-
 // void main() {
 //   runApp(
 //     ChangeNotifierProvider(
@@ -32,7 +30,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // importa o arquivo que você gerou
+import 'firebase_options.dart'; // Importar as configurações geradas
 import 'package:healthclick_app/models/CartProvider.dart';
 import 'package:healthclick_app/screens/auth/Login.dart';
 import 'package:provider/provider.dart';
@@ -40,13 +38,30 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options:
+        DefaultFirebaseOptions.currentPlatform, // Usar as opções do firebase
   );
-
   runApp(
     ChangeNotifierProvider(
       create: (_) => CartProvider(),
-      child: const Login(),
+      child: const MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SaudeClick',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home: const Login(),
+    );
+  }
 }
