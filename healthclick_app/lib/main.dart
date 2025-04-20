812a94e4d2dc74196ab9a8400b6603cfea4fcc5e
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-04-20 20:08:08 +0200

    Doing some updates in the cart provider

--- healthclick_app/lib/main.dart
+++ healthclick_app/lib/main.dart
@@ -1,33 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:healthclick_app/models/CartProvider.dart';
-// import 'package:healthclick_app/screens/auth/Login.dart';
-// import 'package:provider/provider.dart';
-// void main() {
-//   runApp(
-//     ChangeNotifierProvider(
-//       create: (_) => CartProvider(),
-//       child: const MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'SaudeClick',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-//         useMaterial3: true,
-//       ),
-//       home: const Login(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:healthclick_app/ThemeProvider.dart';
--- healthclick_app/lib/models/CartProvider.dart
+++ healthclick_app/lib/models/CartProvider.dart
@@ -1,43 +1,96 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 class CartItem {
+  final String id;
   final String name;
-  final String image;
   final double price;
+  final String image;
+  int quantity;
 
   CartItem({
+    required this.id,
     required this.name,
-    required this.image,
     required this.price,
+    required this.image,
+    this.quantity = 1,
   });
 }
 
 class CartProvider with ChangeNotifier {
-  List<CartItem> _items = [];
+  Map<String, CartItem> _items = {};
 
-  List<CartItem> get items => _items;
-
-  void addItem(CartItem item) {
-    _items.add(item);
-    notifyListeners();
+  Map<String, CartItem> get items {
+    return {..._items};
   }
 
-  void removeItem(CartItem item) {
-    _items.remove(item);
-    notifyListeners();
+  int get itemCount {
+    return _items.length;
   }
 
-  double get totalPrice {
-    double total = 0;
-    for (var item in _items) {
-      total += item.price;
-    }
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
     return total;
   }
 
-  void clearCart() {
-    _items.clear();
+  void addItem(String productId, String name, double price, String image) {
+    if (_items.containsKey(productId)) {
+      // Aumenta a quantidade se o produto já está no carrinho
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          name: existingCartItem.name,
+          price: existingCartItem.price,
+          image: existingCartItem.image,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      // Adiciona novo item
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: productId,
+          name: name,
+          price: price,
+          image: image,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          name: existingCartItem.name,
+          price: existingCartItem.price,
+          image: existingCartItem.image,
+          quantity: existingCartItem.quantity - 1,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
     notifyListeners();
   }
 }
-
